919778f44966520f324407ce13688fc4a4f691d7
Catherine Ochoa <catherineochoa@Catherines-MacBook-Air.local>
2026-02-12 21:01:45 -0500

    Add section identifier in nav bar nothing g ' ''

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logocopy.svg'
+import underline from '../../assets/nav_underline.svg'
 const Navbar = () => {
+    const[menu, setMenu] =useState("home");
     return (
         <div className='navbar'>
             <img src={logo} alt="" />
             <ul className="nav-menu">
-                <li>Home</li>
-                <li>About</li>
-                <li>Services</li>
-                <li>Portfolio</li>
-                <li>Contact</li>
+                <li><p onClick={()=>setMenu("home")}>Home</p>{menu === "home" ? <img src={underline} alt="" />:<></>}</li>
+                <li><p onClick={()=>setMenu("about")}>About</p>{menu === "about" ? <img src={underline} alt="" />:<></>}</li>
+                <li><p onClick={()=>setMenu("services")}>Services</p>{menu === "services" ? <img src={underline} alt="" />:<></>}</li>
+                <li><p onClick={()=>setMenu("portfolio")}>Portfolio</p>{menu === "portfolio" ? <img src={underline} alt="" />:<></>}</li>
+                <li><p onClick={()=>setMenu("contact")}>Contact</p>{menu === "contact" ? <img src={underline} alt="" />:<></>}</li>
             </ul> 
             <div className="nav-connect">Connect With Me</div>
         </div>
