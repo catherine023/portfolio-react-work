c3daceb1f501e246a0251685e3394d372b18d342
Catherine Ochoa <catherineochoa@Catherines-MacBook-Air.local>
2026-03-14 20:58:03 -0400

    close menu icon working in mobile

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logocopy.svg'
 import underline from '../../assets/nav_underline.svg'
@@ -6,17 +6,25 @@ import AnchorLink from 'react-anchor-link-smooth-scroll'
 import menu_open from '../../assets/menu_open.svg'
 import menu_close from '../../assets/menu_close.svg'
 const Navbar = () => { 
-    const[menu, setMenu] =useState("home");
+    const[ menu, setMenu] =useState("home");
+    const menuRef = useRef();
+
+    const openMenu = () => {
+        menuRef.current.style.right="0";
+    }
+     const closeMenu = () => {
+        menuRef.current.style.right="-350px";
+    }
     return (
         <div className='navbar'>
             <img src={logo} alt=" C.O" />
-            <img src={menu_open} alt="" className='nav-mob-open'/>
-            <ul className="nav-menu">
-                <img src={menu_close} alt="" className="nav-mob-close" />
+            <img src={menu_open} onClick={openMenu} alt="" className='nav-mob-open'/>
+            <ul ref = {menuRef} className="nav-menu">
+              <li> <img src={menu_close} onClick={closeMenu} alt="" className="nav-mob-close" /> </li> 
                 <li><AnchorLink className='anchor-link' offset={50}href='#home' ><p onClick={()=>setMenu("home")}>Home</p></AnchorLink>{menu === "home" ? <img src={underline} alt="" />:<></>}</li>
                 <li><AnchorLink className='anchor-link' offset={50}href='#about'><p onClick={()=>setMenu("about")}>About</p></AnchorLink>{menu === "about" ? <img src={underline} alt="" />:<></>}</li>
                 <li><AnchorLink className='anchor-link' offset={50}href='#services'><p onClick={()=>setMenu("services")}>Services</p></AnchorLink>{menu === "services" ? <img src={underline} alt="" />:<></>}</li>
-                <li><AnchorLink className='anchor-link' offset={50}href='#work'><p onClick={()=>setMenu("work")}>Portfolio</p></AnchorLink>{menu === "portfolio" ? <img src={underline} alt="" />:<></>}</li>
+                <li><AnchorLink className='anchor-link' offset={50}href='#work'><p onClick={()=>setMenu("work")}>Portfolio</p></AnchorLink>{menu === "work" ? <img src={underline} alt="" />:<></>}</li>
                 <li><AnchorLink className='anchor-link' offset={50}href='#contact'><p onClick={()=>setMenu("contact")}>Contact</p></AnchorLink>{menu === "contact" ? <img src={underline} alt="" />:<></>}</li>
             </ul> 
             <AnchorLink className='anchor-link nav-connect' offset={50} href='#contact'>Connect With Me</AnchorLink>
