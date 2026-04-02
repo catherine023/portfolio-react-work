d975d6a9b5f405a158ee063d3dc2bce38fbf7014
Catherine Ochoa <catherineochoa@catherines-air.myfiosgateway.com>
2026-04-02 12:49:31 -0400

    footer responsive

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,30 +6,32 @@ import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Footer = () => {
   return (
-    <div className='footer'>
-      <div className="footer-top">
-        <div className="footer-top-left">
-          <img src={footer_logoo} alt="" />
-          <p>I am a frontend developer from, USA with 9 months of experience</p>
-        </div>
-        <div className="footer-top-right">
-          <div className="footer-email-input">
-            <img src={user_icon} alt="" />
-            <input type="email" placeholder='Enter your email'/>
-          </div>
-        <div className="footer-subscribe">Subscribe</div>
-          </div>
-        </div>
-        <hr />
-        <div className="footer-bottom">
-        <p className="footer-bottom-left"> © 2025 Catherine Ochoa. All rights reserved.</p>
-       <div className="footer-bottom-right">
-        <p>Term of Services</p>
-        <p>Privacy Policy</p>
-        <p>Connect with Me</p>
-          </div>
-        </div>
+    // ✅ Fixed
+<div className='footer'>
+  <div className="footer-top">
+    <div className="footer-top-left">
+      <img src={footer_logoo} alt="" />
+      <p>I am a frontend developer from, USA with 9 months of experience</p>
+    </div>
+    <div className="footer-top-right">
+      <div className="footer-email-input">
+        <img src={user_icon} alt="" />
+        <input type="email" placeholder='Enter your email'/>
       </div>
+      <div className="footer-subscribe">Subscribe</div>
+    </div>
+  </div>  {/* ← footer-top closes HERE */}
+
+  <div className="footer-bottom">  {/* ← now a sibling, not a child */}
+    <p className="footer-bottom-left">© 2025 Catherine Ochoa. All rights reserved.</p>
+    <div className="footer-bottom-right">
+      <p>Term of Services</p>
+      <p>Privacy Policy</p>
+      <p>Connect with Me</p>
+    </div>
+  </div>
+</div>
+    
   )
 }
 
